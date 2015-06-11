6c6422ec44222f6bab33aeb232765717ca669152
Boyi Lee <boylee1111@gmail.com>
2015-06-11 11:20:09 -0700

    add missing testing file

---
+++ tests/codeception/frontend/acceptance/ReturningOrderAccpetanceCept.php
@@ -0,0 +1,3 @@
+<?php use tests\codeception\frontend\AcceptanceTester;
+$I = new AcceptanceTester($scenario);
+$I->wantTo('perform actions and see result');
---
+++ tests/codeception/frontend/functional/RequirementFunctionalCest.php
@@ -0,0 +1,19 @@
+<?php
+namespace tests\codeception\frontend;
+use tests\codeception\frontend\FunctionalTester;
+
+class RequirementFunctionalCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
